13c2125da45e626151295e0e2d4bb553bb672546
Matthew Franglen <matthew@franglen.org>
2015-03-01 21:22:23 +0000

    Initial code commit
    Need tests!

---
+++ lib/polymer_dependency_injection.dart
@@ -0,0 +1,11 @@
+library polymer_dependency_injection;
+
+import 'dart:html';
+import 'package:polymer/polymer.dart';
+import 'package:annotate/annotate.dart';
+import 'package:patch_mirrors/patch_mirrors.dart';
+import 'package:dependency_injection/dependency_injection.dart';
+
+part 'src/polymer_dependency_injection.dart';
+
+// vim: set ai et sw=2 syntax=dart :
