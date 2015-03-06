fc96460010bea1016f369196c0c2f52f3b03a0ba
Matthew Franglen <matthew@franglen.org>
2015-03-06 11:55:39 +0000

    Remove dependency on patch_mirrors
    Not actually used directly.

--- lib/polymer_dependency_injection.dart
+++ lib/polymer_dependency_injection.dart
@@ -3,7 +3,6 @@ library polymer_dependency_injection;
 import 'dart:html';
 import 'package:polymer/polymer.dart';
 import 'package:annotate/annotate.dart';
-import 'package:patch_mirrors/patch_mirrors.dart';
 import 'package:dependency_injection/dependency_injection.dart';
 
 part 'src/polymer_dependency_injection.dart';
