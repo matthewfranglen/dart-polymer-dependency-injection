55d0488e188d03890a93c8fce06f331077364fd5
Matthew Franglen <matthew@franglen.org>
2015-03-06 11:56:09 +0000

    Re-export the dependency_injection library
    This is really a wrapper around that.

--- lib/polymer_dependency_injection.dart
+++ lib/polymer_dependency_injection.dart
@@ -5,6 +5,8 @@ import 'package:polymer/polymer.dart';
 import 'package:annotate/annotate.dart';
 import 'package:dependency_injection/dependency_injection.dart';
 
+export 'package:dependency_injection/dependency_injection.dart';
+
 part 'src/polymer_dependency_injection.dart';
 
 // vim: set ai et sw=2 syntax=dart :
